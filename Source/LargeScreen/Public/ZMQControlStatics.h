ea7fbbfeb6af52c01136b248219aa2d79e487d37
wuhao <810238014@qq.com>
2025-10-15 09:20:45 +0800

    创建插件

---
+++ Source/LargeScreen/Public/ZMQControlStatics.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "ZMQControlStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZMQCONTROL_API UZMQControlStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
